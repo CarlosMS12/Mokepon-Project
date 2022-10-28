e5ebed22a77ad21c052fd53a5a4c3bdfb970839a
CarlosMS12 <carlosmagallanes207@gmail.com>
2022-10-28 15:39:34 -0500

    function selecionarMascota add

--- mokepon/js/mokepon.js
+++ mokepon/js/mokepon.js
@@ -3,8 +3,29 @@ function iniciarJuego(){
 
     botonMascotaJugador.addEventListener("click", seleccionarMascotaJugador)
 }
-function seleccionarMascotaJugador(){
-    alert("Seleccionaste tu mascota")
+function seleccionarMascotaJugador() {
+    let inputHipodoge = document.getElementById("hipodoge")
+    let inputCapipepo = document.getElementById("capipepo")
+    let inputRatigueya = document.getElementById("ratigueya")
+    let inputLangostelvis = document.getElementById("langostelvis")
+    let inputTucapalma = document.getElementById("tucapalma")
+    let inputPydos = document.getElementById("pydos")
+
+    if (inputHipodoge.checked){
+        alert("Seleccionaste a hipodoge")
+    } else if (inputCapipepo.checked) {
+        alert("Seleccionaste a Capipepo")
+    } else if (inputRatigueya.checked) {
+        alert("Seleccionaste a Ratigueya")
+    } else if (inputLangostelvis.checked) {
+        alert("Seleccionaste a Langostelvis")
+    }  else if (inputTucapalma.checked) {
+        alert("Seleccionaste a Tucapalma") 
+    } else if (inputPydos.checked) {
+        alert("Seleccionaste a Pydos")
+    } else {
+        alert("Selecciona a tu mascota")
+    }
 }
 
 window.addEventListener("load", iniciarJuego)
