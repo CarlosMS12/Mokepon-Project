97e20c4eff7149d6e39d81a271f29275298facf5
CarlosMS12 <carlosmagallanes207@gmail.com>
2022-11-02 14:56:50 -0600

    Ocultando secciones con js

--- mokepon/js/mokepon.js
+++ mokepon/js/mokepon.js
@@ -4,7 +4,15 @@ let vidasJugador = 3
 let vidasEnemigo = 3
 
 function iniciarJuego(){
+    let sectionSeleccionarAtaque = document.getElementById("seleccionar-ataque")
+    
+    let sectionReiniciar = document.getElementById("reiniciar")
+    sectionReiniciar.style.display = "none"
+
+    sectionSeleccionarAtaque.style.display = "none"
     let botonMascotaJugador = document.getElementById("boton-mascota")
+
+    
 
     botonMascotaJugador.addEventListener("click", seleccionarMascotaJugador)
 
@@ -20,6 +28,10 @@ function iniciarJuego(){
 
 }
 function seleccionarMascotaJugador() {
+    let sectionSeleccionarMascota = document.getElementById("seleccionar-mascota")
+    sectionSeleccionarMascota.style.display = "none"
+    let sectionSeleccionarAtaque = document.getElementById("seleccionar-ataque")
+    sectionSeleccionarAtaque.style.display = "block"
     let inputHipodoge = document.getElementById("hipodoge")
     let inputCapipepo = document.getElementById("capipepo")
     let inputRatigueya = document.getElementById("ratigueya")
@@ -113,6 +125,9 @@ function crearMensajeFinal(resultadoFinal) {
     botonAgua.disabled = true
     let botonTierra = document.getElementById("boton-tierra")
     botonTierra.disabled = true
+
+    let sectionReiniciar = document.getElementById("reiniciar")
+    sectionReiniciar.style.display = "block"
 }
 function combate () {
     let spanVidasJugador = document.getElementById("vidas-jugador")
